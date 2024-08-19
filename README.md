f3c1bf26ff01bc46529ccb0ad07bced321ef3b30
yasAC19 <106816015+yasandradeRJ@users.noreply.github.com>
2024-08-19 11:29:41 +0000

    Terminando

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 **Programação II**
 
-Bora começar o primeiro servidor com rotas!
+Bora começar o primeiro servidor com um tutorial top das galáxias!
 
 # Iniciando um repositório no CodeSpace
 Crie um "repositório" nomeando com "seu nome-IA24".
+No seu git hub, na parte de cima do canto direito, tem uma caixinha com um "+"(mais) dentro, aperte la, e depois em `"new repository"`.
 
 *Lembre: o repositório deve ser com o arquivo README incluso.
 
@@ -18,6 +19,18 @@ Aqui fica o textinho
 ````javascript
 Codigo aqui
 ````
+# Iniciando um repositório no CodeSpace
+Crie um "repositório" nomeando com "seu nome-IA24".
+
+*Lembre: o repositório deve ser com o arquivo README incluso.
+
+Agora, clique no botão `code` e depois em `codespace on main`, espere aparecer o "gatinho" do github para continuar os próximos passos 
+
+
+# Titulo
+Aqui fica o textinho
+
+
 
 `destaca`
 **destaca2**
--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname + '/../public'))
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -27,6 +28,26 @@ app.get('/users', async (req, res) => {
   const users = await db.all('SELECT * FROM users');
 
   res.json(users);
+});
+
+app.put('/users/:id', async (req, res) => {
+  const db = await connect();
+  const { name, email } = req.body;
+  const { id } = req.params;
+
+  await db.run('UPDATE users SET name = ?, email = ? WHERE id = ?', [name, email, id]);
+  const user = await db.get('SELECT * FROM users WHERE id = ?', [id]);
+
+  res.json(user);
+});
+
+app.delete('/users/:id', async (req, res) => {
+  const db = await connect();
+  const { id } = req.params;
+
+  await db.run('DELETE FROM users WHERE id = ?', [id]);
+
+  res.json({ message: 'User deleted' });
 });
 
 app.listen(port, () => {
